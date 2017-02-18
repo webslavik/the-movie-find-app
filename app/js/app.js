9785b5e36676633e1f22f904603e92f9732de8ac
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-02-18 13:54:35 +0200

    include ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,32 @@
 
 	'use strict';
 
-	angular.module('app', ['ngMaterial'])
-		.config(($mdThemingProvider, $mdIconProvider) => {
+	angular.module('app', ['ngMaterial', 'ui.router'])
+
+
+	angular.module('app').config(($stateProvider) => {
+
+		$stateProvider
+			.state('themovie', {
+				url: '',
+				abtsract: true,
+				views: {
+					'toolbar': {
+						templateUrl: 'views/toolbar.html'
+					},
+					'cards-list': {
+						templateUrl: 'views/cards-list.html'
+					},
+					'about-button': {
+						templateUrl: 'views/about-button.html'
+					}
+				}
+			});
+
+	});
+
+
+	angular.module('app').config(($mdThemingProvider, $mdIconProvider) => {
 			$mdThemingProvider.theme('default')
 		    .primaryPalette('blue')
 		    .accentPalette('pink');
